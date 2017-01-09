8c82b87cb7d3b98107f2127754d4f5bc95fa258e
jackbenimbo <brinder.dhaliwal13@gmail.com>
2017-01-09 20:31:32 +0000

    Update business logic in model to only show posts released today or any time before

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    
+    #All business logic is in controller. For example, lets say in the future, I only want posts to show that were
+    #..released between a particular set of months or only ones that will be released in the future, I can change the logic here.
+    
+    def self.released
+        where("published <= ?", Time.now).order("published desc")
+    end
 end
