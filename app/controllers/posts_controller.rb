21261058372858c1e82e65f2caaf157f7a64f51e
jackbenimbo <brinder.dhaliwal13@gmail.com>
2017-01-09 19:58:51 +0000

    Added Forms, Cleaned up Actions and Routes, Creating new branch for layouts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,30 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+	
+	def edit
+		@post = Post.find(params[:id])
+	end
+	
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to @post
+	end
+	
+	def new
+		@post = Post.new
+	end
+	
+	def create
+		@post = Post.new(post_params)
+		@post.save
+		redirect_to @post
+	end
 end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :category, :published, :read_length)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "posts" => "posts#index"
-  get "posts/:id" => "posts#show"
+  
+  root "posts#index"
+  resources :posts 
 end
