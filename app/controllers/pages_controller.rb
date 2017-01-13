1f194abc4b543d1323fc145f8c80f521c02de7da
jackbenimbo <brinder.dhaliwal13@gmail.com>
2017-01-13 04:42:32 +0000

    add pages controller and about page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+    def about
+    end
+    
+    def show
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   
   root "posts#index"
   resources :posts 
+  
+  #pages routes
+  get "/pages/about", to: "pages#about"
 end
