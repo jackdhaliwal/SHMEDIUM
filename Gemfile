8a804f92078e4f1295170a4b8880f2f31089964d
jackbenimbo <brinder.dhaliwal13@gmail.com>
2017-06-25 08:13:43 +0000

    disable node_modules cache

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+#ruby 'ruby 2.3.0p0'
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
@@ -75,6 +76,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'npm-pipeline-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,38 @@ module Blog
     config.autoload_paths += %W(#{config.root}/lib)
 
   end
+
+Rails.application.configure do
+  # Enables npm_pipeline_rails's invocation of `watch` commands. (v1.5.0+)
+  # If `true`, watch commands will be ran alongside Rails's server.
+  # Defaults to true in development.
+  config.npm.enable_watch = Rails.env.development?
+
+  # Command to install dependencies
+  config.npm.install = ['npm install']
+
+  # Command to build production assets
+  config.npm.build = ['npm run build']
+
+  # Command to start a file watcher
+  config.npm.watch = ['npm run start']
+
+  # The commands are arrays; you may add more commands as needed:
+  config.npm.watch = [
+    'npm run webpack:start',
+    'npm run brunch:start'
+  ]
+
+  # If 'true', runs 'npm install' on 'rake assets:precompile'. (v1.6.0+)
+  # If you disable this, you'll need to run `npm install` yourself.
+  # This is generally desired, but you may set this to false when
+  # deploying to Heroku to speed things up.
+  config.npm.install_on_asset_precompile = true
+
+  # If 'true', runs 'npm install' on 'rails server'. (v1.7.0+)
+  # If you disable this, you'll need to run `npm install` yourself.
+  config.npm.install_on_rails_server = true
+end
+
+
 end
