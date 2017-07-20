c4d33110755de99ee1ca5f5455d650377719d481
jackbenimbo <brinder.dhaliwal13@gmail.com>
2017-07-20 23:50:00 +0000

    mailer works, kind of

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :mailer_set_url_options
   
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
   end
   
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
   
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'shmedium.notifications@gmail.com'
   layout 'mailer'
 end
